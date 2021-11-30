1bf2df99d385745a40ac861544759a9a94c00a7d
jvieroe <43220957+jvieroe@users.noreply.github.com>
2021-11-30 17:49:22 +0100

    cross_breeze

---
+++ R/cross_breeze.R
@@ -0,0 +1,64 @@
+#' Calculate
+#'
+#' This function
+#'
+#' @param data xx
+#' @param keep_character xx. Defaults to `FALSE`
+#' @return lorem ipsum
+#' @author Jeppe Vierø
+#' @import dplyr tibble labelled rlang janitor
+#' @export
+
+cross_breeze <- function(data = NULL,
+                         cross_var = NULL,
+                         keep_character = FALSE) {
+
+  validate_cross_breeze(data = data,
+                        keep_character = keep_character)
+
+  vars <- df %>%
+    base::names(.) %>%
+    tibble::tibble() %>%
+    dplyr::rename(variable = '.')
+
+  temp <- df %>%
+    labelled::var_label(.) %>%
+    base::unlist() %>%
+    tibble::tibble() %>%
+    dplyr::rename(label = '.')
+
+  # introduces error if 1+ variable has no label. Fix!
+
+  labs <- base::cbind(vars, temp)
+
+  all_vars <- df %>%
+    base::names(.)
+
+  tab_list_temp <- base::list()
+
+  for(i in base::seq_along(all_vars)) {
+
+    temp <- df %>%
+      dplyr::rename(var := !!rlang::sym(all_vars[i]))
+
+    tab_temp <- janitor::tabyl(temp$var) %>%
+      dplyr::mutate(variable = all_vars[i]) %>%
+      dplyr::rename(var = 'temp$var') %>%
+      dplyr::filter(!is.na(var)) %>%
+      dplyr::group_by(variable) %>%
+      dplyr::mutate(n_count = sum(n,
+                                  na.rm = T)) %>%
+      dplyr::mutate(pct = n/n_count) %>%
+      dplyr::mutate(pct_validation = sum(pct,
+                                         na.rm = T)) %>%
+      dplyr::ungroup()
+
+    tab_list_temp[[i]] <- tab_temp
+
+  }
+
+  tab_data_exp <- dplyr::bind_rows(tab_list_temp) %>%
+    dplyr::left_join(labs,
+                     by = c("variable"))
+
+}
